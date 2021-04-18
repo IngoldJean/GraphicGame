d3c236fdac53333ebfbdd63b3c0bb667f73bac3f
IngoldJean <32537089+IngoldJean@users.noreply.github.com>
2021-04-18 17:01:35 +0200

    create the first screen with centered button and text

--- src/main/scala/EntryScreen.scala
+++ src/main/scala/EntryScreen.scala
@@ -1,8 +1,9 @@
-import javafx.geometry.VPos
-import javafx.scene.{Group, Scene}
-import javafx.scene.layout.{Pane, VBox}
-import javafx.scene.shape.Line
-import javafx.scene.text.{Font, FontPosture, FontWeight, Text, TextAlignment}
+import javafx.geometry.{HPos, Insets, Pos}
+import javafx.scene.Scene
+import javafx.scene.control.Button
+import javafx.scene.layout.GridPane
+import javafx.scene.paint.Color
+import javafx.scene.text._
 import javafx.stage.Stage
 
 class EntryScreen extends javafx.application.Application{
@@ -10,22 +11,35 @@ class EntryScreen extends javafx.application.Application{
   val width = 500
   val height  = 300
 
-  def drawEntryScreen(root:Group): Unit = {
+  def drawEntryScreen(root:GridPane): Unit = {
 
-    val welcomeText = new Text("Hello World")
+    val welcomeText = new Text("Welcome to my graphical app")
 
     welcomeText.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.ITALIC, 20))
-    welcomeText.setX(250)
-    welcomeText.setY(150)
-    root.getChildren.add(welcomeText)
+    welcomeText.setFill(Color.ALICEBLUE)
+
+    val welcomeButton = new Button("Click to enter")
+
+    root.setPadding(new Insets(10, 10, 10, 10))
+    root.setVgap(5)
+    root.setHgap(5)
+    root.setAlignment(Pos.CENTER)
+    root.setStyle("-fx-background-color: BLACK;")
+
+    GridPane.setHalignment(welcomeText, HPos.CENTER)
+    GridPane.setHalignment(welcomeButton, HPos.CENTER)
+
+    root.add(welcomeText, 0, 0)
+    root.add(welcomeButton, 0, 1)
   }
 
   override def start(primaryStage: Stage): Unit = {
     primaryStage.setTitle("Entry Screen");
-    val root = new Group()
-    primaryStage.setScene(new Scene(root, width, height))
+    val gridPane = new GridPane
+    val scene = new Scene(gridPane, width, height)
+    primaryStage.setScene(scene)
     primaryStage.show()
-    //ici on appelle la fonction qui dessine sur le 'root'
-    drawEntryScreen(root)
+
+    drawEntryScreen(gridPane)
   }
 }
