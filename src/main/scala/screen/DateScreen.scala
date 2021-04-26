9b95bbef480cea9480664293aab5d8a7c2c625bd
IngoldJean <32537089+IngoldJean@users.noreply.github.com>
2021-04-26 23:59:42 +0200

    had a header

--- src/main/scala/screen/DateScreen.scala
+++ src/main/scala/screen/DateScreen.scala
@@ -4,7 +4,6 @@ import api.model.FullTimeSerieJson
 import api.timeserie.ReadTimeSerie
 import common.DateManager
 import io.circe.parser
-import javafx.scene.Node
 import javafx.scene.layout.GridPane
 import javafx.scene.paint.Color
 import javafx.scene.text.Text
@@ -15,7 +14,7 @@ import scala.io.Source
 
 class DateScreen {
 
-  var number = 1
+  var rowCurrentNumber = 1
 
   def readDataFile: String => Seq[String] = {
     fileName =>
@@ -24,6 +23,19 @@ class DateScreen {
         yield line
 
       lines.toSeq
+  }
+
+  def createHeader(root: GridPane): Unit = {
+    val nameOfThePositionHeader = new Text("Position name")
+    val numberOfStockHeader = new Text("Number of unit")
+    val closeValueStockHeader = new Text("Value at close")
+    val sumOfValueHeader = new Text("Total Value")
+    nameOfThePositionHeader.setFill(Color.ALICEBLUE)
+    numberOfStockHeader.setFill(Color.ALICEBLUE)
+    closeValueStockHeader.setFill(Color.ALICEBLUE)
+    sumOfValueHeader.setFill(Color.ALICEBLUE)
+    root.addRow(rowCurrentNumber, nameOfThePositionHeader, numberOfStockHeader, closeValueStockHeader, sumOfValueHeader)
+    rowCurrentNumber += 1
   }
 
   def createStockRow(root: GridPane): Position => Unit = {
@@ -41,18 +53,21 @@ class DateScreen {
       closeValueStock.setFill(Color.ALICEBLUE)
       sumOfValue.setFill(Color.ALICEBLUE)
 
-      root.addRow(number, nameOfThePosition, numberOfStock, closeValueStock, sumOfValue)
-      number += 1
+      root.addRow(rowCurrentNumber, nameOfThePosition, numberOfStock, closeValueStock, sumOfValue)
+      rowCurrentNumber += 1
   }
 
   def drawDateScreen(root:GridPane): Unit = {
+
     val dateText = new Text(DateManager.getTheDateString)
     dateText.setFill(Color.ALICEBLUE)
+
+    createHeader(root)
 
     val lines = PositionsReader.readPositionsFromFile("/Users/Jean/IdeaProjects/GraphicGame/src/main/resources/data.txt")
 
     lines.foreach(createStockRow(root))
 
-    root.add(dateText,0,0)
+    root.add(dateText,0,0, 4, 1)
   }
 }
