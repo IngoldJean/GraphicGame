bf4eebaa48f2a275615aa5484b31bf23f0dcbe38
IngoldJean <32537089+IngoldJean@users.noreply.github.com>
2021-04-26 23:42:54 +0200

    get the total value and modify the way to get the fill the data

--- src/main/scala/screen/DateScreen.scala
+++ src/main/scala/screen/DateScreen.scala
@@ -3,18 +3,19 @@ package screen
 import api.model.FullTimeSerieJson
 import api.timeserie.ReadTimeSerie
 import common.DateManager
-import io.circe
 import io.circe.parser
+import javafx.scene.Node
 import javafx.scene.layout.GridPane
 import javafx.scene.paint.Color
 import javafx.scene.text.Text
+import positions.model.Position
 import positions.reader.PositionsReader
 
-import java.time.LocalDateTime
-import java.time.format.DateTimeFormatter
 import scala.io.Source
 
 class DateScreen {
+
+  var number = 1
 
   def readDataFile: String => Seq[String] = {
     fileName =>
@@ -25,32 +26,32 @@ class DateScreen {
       lines.toSeq
   }
 
+  def createStockRow(root: GridPane): Position => Unit = {
+    stockLine =>
+      val nameOfThePosition = new Text(stockLine.name)
+      val numberOfStock = new Text(stockLine.numberOfStock.toString)
+      nameOfThePosition.setFill(Color.ALICEBLUE)
+      numberOfStock.setFill(Color.ALICEBLUE)
+
+      val (closeValueStock, sumOfValue) = parser.decode[FullTimeSerieJson](ReadTimeSerie.timeSerieDaily(stockLine.name)) match {
+        case Right(a) => val closeValue = a.datas.todayValue.close
+          (new Text(closeValue.toString), new Text((closeValue * stockLine.numberOfStock).toString))
+        case Left(_) => (new Text("Unable to read API for that value"), new Text("Unable to read API for that value"))
+      }
+      closeValueStock.setFill(Color.ALICEBLUE)
+      sumOfValue.setFill(Color.ALICEBLUE)
+
+      root.addRow(number, nameOfThePosition, numberOfStock, closeValueStock, sumOfValue)
+      number += 1
+  }
+
   def drawDateScreen(root:GridPane): Unit = {
     val dateText = new Text(DateManager.getTheDateString)
     dateText.setFill(Color.ALICEBLUE)
 
     val lines = PositionsReader.readPositionsFromFile("/Users/Jean/IdeaProjects/GraphicGame/src/main/resources/data.txt")
 
-    var rowIndex = 1
-
-    for(line <- lines){
-      val nameOfThePosition = new Text(line.name)
-      val numberOfStock = new Text(line.numberOfStock.toString)
-      nameOfThePosition.setFill(Color.ALICEBLUE)
-      numberOfStock.setFill(Color.ALICEBLUE)
-
-      val closeValueStock = parser.decode[FullTimeSerieJson](ReadTimeSerie.timeSerieDaily(line.name)) match {
-        case Right(a) => new Text(a.datas.todayValue.close.toString)
-        case Left(a) => new Text("Unable to read API for that value")
-      }
-
-      closeValueStock.setFill(Color.ALICEBLUE)
-
-      root.add(nameOfThePosition, 0, rowIndex)
-      root.add(numberOfStock, 1, rowIndex)
-      root.add(closeValueStock, 2, rowIndex)
-      rowIndex += 1
-    }
+    lines.foreach(createStockRow(root))
 
     root.add(dateText,0,0)
   }
